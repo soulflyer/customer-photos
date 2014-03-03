fd924273bd0512fb896a5f2bbfc57831a48fe954
Iain <public@soulflyer.co.uk>
2014-03-03 19:41:52 +0700

    3types working

--- bin/build-web-page.sh
+++ bin/build-web-page.sh
@@ -14,13 +14,13 @@ cat <<EOF > $PAGE
 <!doctype html>
 <html>
 <head>
-  <link rel="stylesheet" type="text/css" href="/customerphotos/css/main.css">
-  <link rel="stylesheet" media="(max-width: 600px)"  type="text/css" href="/customerphotos/css/phone.css">
-  <link rel="stylesheet" media="(max-width: 1024px)" type="text/css" href="/customerphotos/css/touch-screen.css">
-  <link rel="stylesheet" media="(min-width: 601px) and (max-width: 1024px)" type="text/css" href="/customerphotos/css/tablet.css">
-  <link rel="stylesheet" media="(min-width: 1024px)" type="text/css" href="/customerphotos/css/laptop.css">
-  <script type="text/javascript" src="/customerphotos/js/jquery-2.0.3.js"></script>
-  <script type="text/javascript" chset="utf-8" src="/customerphotos/js/mobile-jquery.js"></script>
+  <link rel="stylesheet" type="text/css" href="/divephotos/css/main.css">
+  <link rel="stylesheet" media="(max-width: 600px)"  type="text/css" href="/divephotos/css/phone.css">
+  <link rel="stylesheet" media="(max-width: 1024px)" type="text/css" href="/divephotos/css/touch-screen.css">
+  <link rel="stylesheet" media="(min-width: 601px) and (max-width: 1024px)" type="text/css" href="/divephotos/css/tablet.css">
+  <link rel="stylesheet" media="(min-width: 1024px)" type="text/css" href="/divephotos/css/laptop.css">
+  <script type="text/javascript" src="/divephotos/js/jquery-2.0.3.js"></script>
+  <script type="text/javascript" chset="utf-8" src="/divephotos/js/mobile-jquery.js"></script>
 
 </head>
 <body>
